37a469814662add578f696dec392298d1ce0c57e
Dioney Gabriel <62361235+SimpleDioney@users.noreply.github.com>
2024-07-16 15:31:50 -0300

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,18 +1,1 @@
-function extractVideoId(url) {
-            const regExp = /(?:view_video.php\?viewkey=|embed\/)([^&\/#\?]*)/;
-            const match = url.match(regExp);
-            return match ? match[1] : false;
-        }
-
-        function embedVideo() {
-            const url = document.getElementById('videoUrlInput').value;
-            const videoContainer = document.getElementById('videoContainer');
-            const videoId = extractVideoId(url);
-
-            if (videoId) {
-                const embedCode = `<iframe src="https://www.pornhub.com/embed/${videoId}" frameborder="0" width="560" height="340" scrolling="no" allowfullscreen></iframe>`;
-                videoContainer.innerHTML = embedCode;
-            } else {
-                videoContainer.innerHTML = '<p>URL inválida. Por favor, verifique e tente novamente.</p>';
-            }
-        }
+oi
