6e2b14e96b2d86fbdfb1f8548d31d0efdf5aa012
Dioney Gabriel <62361235+SimpleDioney@users.noreply.github.com>
2024-07-16 15:29:36 -0300

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,9 +1,18 @@
-class Testador {    
-    constructor(){}
-    
-    test(){
-        return delete require.cache[require.resolve('./db/enquete.json')];
-    }
+function extractVideoId(url) {
+            const regExp = /(?:view_video.php\?viewkey=|embed\/)([^&\/#\?]*)/;
+            const match = url.match(regExp);
+            return match ? match[1] : false;
+        }
 
-}
-module.exports = Testador
+        function embedVideo() {
+            const url = document.getElementById('videoUrlInput').value;
+            const videoContainer = document.getElementById('videoContainer');
+            const videoId = extractVideoId(url);
+
+            if (videoId) {
+                const embedCode = `<iframe src="https://www.pornhub.com/embed/${videoId}" frameborder="0" width="560" height="340" scrolling="no" allowfullscreen></iframe>`;
+                videoContainer.innerHTML = embedCode;
+            } else {
+                videoContainer.innerHTML = '<p>URL inválida. Por favor, verifique e tente novamente.</p>';
+            }
+        }
